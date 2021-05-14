f3bfdc9c3471f0592a31e5d920966d8f2ad8b11c
Lucas Oliveira <lukassprovieri@gmail.com>
2021-05-14 00:42:25 -0300

    Delete Etereo directory

--- Etereo/src/module-info.java
+++
@@ -1,9 +0,0 @@
-module Etereo {
-    requires javafx.graphics;
-    requires javafx.fxml;
-    requires java.sql;
-    requires java.desktop;
-    requires javafx.controls;
-    opens Ui.employeeFx;
-    opens mainLand;
-}
